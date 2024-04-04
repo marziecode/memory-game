7b98df39a29a6a2582bd7bc6a379f8ac5a6686a5
marziecode <marzieh2802@gmail.com>
2024-04-04 17:50:52 +0330

    try to fix condition

--- src/Cards.tsx
+++ src/Cards.tsx
@@ -14,21 +14,30 @@ type SelectedCard = {
   name: string;
 };
 
+// const hasCard = selectedId.some((i) => i.id !== id);
+
+// console.log(hasCard, "hascard");
+// if (hasCard) {
+//   setSelectedId((prev) => [...prev, { id, name }]);
+// } else {
+//   //problem is here
+//   setSelectedId([{ id, name }]);
+// }
+
 function Cards() {
   const [selectedId, setSelectedId] = useState<SelectedCard[]>([]);
 
   console.log(selectedId, "selectedid");
 
   const handelGetIdx = (id: number, name: string) => {
-    const hasCard = selectedId.some((i) => i.id !== id);
-
-    console.log(hasCard, "hascard");
-    if (hasCard) {
+    const x = selectedId.some((i) => i.id !== id);
+    if (x) {
       setSelectedId((prev) => [...prev, { id, name }]);
     } else {
-      //problem is here
       setSelectedId([{ id, name }]);
     }
+    console.log(x, "x");
+    console.log(selectedId, "sss");
   };
 
   useEffect(() => {}, []);
