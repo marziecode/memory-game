05e219ef597cf66d5e335834599fc15668b5ca05
marziecode <marzieh2802@gmail.com>
2024-04-17 01:11:24 +0330

    fix all the styles

--- src/Cards.tsx
+++ src/Cards.tsx
@@ -1,6 +1,8 @@
 import { CARDS_ITEMS } from "./constants";
-import cover from "./assets/cover-card.png";
+import cover from "./assets/cover-img.png";
 import { useEffect, useState } from "react";
+import vectore1 from "./assets/vectore-1.png";
+import vectoreLeft from './assets/vectore-left.png'
 
 type SelectedCardType = {
   id: number;
@@ -63,7 +65,7 @@ function Cards() {
     if (selectedCard.length !== 0) {
       handleCalculateWin();
     }
-    if (loosCount >= 2) {
+    if (loosCount >= 4) {
       setTimeout(() => {
         handleRefresh();
       }, 2000);
@@ -75,8 +77,8 @@ function Cards() {
   }, []);
 
   return (
-    <div className="flex flex-col justify-center items-center w-full h-screen bg-gradient-to-r from-[#525252] to-[#3d72b4] p-[35px] overflow-hidden ">
-      {loosCount >= 3 && (
+    <div className="flex flex-col justify-center items-center w-full h-screen bg-gradient-to-r from-[#386f53] to-[#6aaa6e] p-[35px] overflow-hidden ">
+      {loosCount >= 4 && (
         <div className="absolute bg-red-500 w-[400px] h-[200px] flex flex-col justify-center items-center text-2xl text-white rounded-[25px] opacity-95">
           <p>GAME OVER !</p>
           <button className="text-2xl text-white my-8" onClick={handleRefresh}>
@@ -85,30 +87,30 @@ function Cards() {
         </div>
       )}
       <div className="w-full h-[80px] flex justify-center items-center ">
-        <div className="w-[100px] h-[30px]  flex justify-center items-center rounded-full  bg-[#38359788]  border border-[#d4d4e063]">
+        <div className="w-[100px] h-[30px]  flex justify-center items-center rounded-full  bg-[#476e62]  border border-[#26322363] ">
           <p className="text-1xl text-[#fff] font-bold my-3">
             WIN : {countWin}
           </p>
         </div>
-        <div className="ml-[15px] w-[100px] h-[30px]  flex justify-center items-center rounded-full  bg-[#f74242be] border border-[#d4d4e063]">
+        <div className="ml-[15px] w-[100px] h-[30px]  flex justify-center items-center rounded-full  bg-[#f76b83] border border-[#d4d4e063] ">
           <p className="text-1xl text-[#fff] font-bold my-6">
             Loose : {loosCount}
           </p>
         </div>
 
         <button
-          className=" ml-[15px] text-1xl text-white w-[100px] h-[30px]  flex justify-center items-center rounded-full  bg-[#38359788] cursor-pointer hover:bg-blue-700 border border-[#d4d4e063]"
+          className=" ml-[15px] text-1xl text-white w-[100px] h-[30px]  flex justify-center items-center rounded-full  bg-[#476e62] cursor-pointer hover:bg-[#66d8a6] border border-[#26322363]"
           onClick={handleRefresh}
         >
           Refresh
         </button>
       </div>
 
-      <div className="w-[850px] h-[540px] rounded-3xl bg-[#070f2b87] flex justify-center items-center flex-wrap mt-2">
+      <div className="w-[800px] h-[530px] rounded-3xl bg-[#96e3a244] flex justify-center items-center flex-wrap shadow-2xl	">
         {randomCardData?.map((i, idx) => (
           <div
             key={idx}
-            className="w-[200px] cursor-pointer h-[280px] flex justify-center items-center"
+            className="w-[200px] cursor-pointer h-[210px] flex justify-center items-center "
             onClick={() => handelGetIdx(i?.id, i?.name)}
           >
             <img
@@ -119,10 +121,20 @@ function Cards() {
                   ? i?.image
                   : cover
               }
-              className="w-[145px]"
+              className="w-[140px]"
             />
           </div>
         ))}
+        <img
+          src={vectore1}
+          alt=""
+          className="w-[100px] absolute right-[265px] bottom-7"
+        />
+        <img
+          src={vectoreLeft}
+          alt=""
+          className="w-[50px] absolute left-[265px] bottom-7"
+        />
       </div>
     </div>
   );
